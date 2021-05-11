2a863868b8c96a9ca943a9cb79aa4e9fc40bff3f
them4d <29798133+them4d@users.noreply.github.com>
2021-05-11 05:07:28 +0300

    ARGB color code snippet
    Signed-off-by: them4d <29798133+them4d@users.noreply.github.com>

---
+++ misc.lua
@@ -0,0 +1,11 @@
+local floor = math.floor
+
+--- Gets the 32-bit ARGB value of given color components
+---@param a integer Alpha
+---@param r integer Red
+---@param g integer Green
+---@param b integer Blue
+---@return integer color 32-bit ARGB value
+local function argb(a, r, g, b)
+    return a*0x1000000 + r*0x10000 + g*0x100 + b
+end
